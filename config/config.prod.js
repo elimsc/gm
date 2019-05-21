bca44881669758f93d859b5c39b95678c525d0f0
elimsc <zhjiang.rtc@gmail.com>
2019-05-21 21:04:08 +0800

    db enviroment

--- config/config.prod.js
+++ config/config.prod.js
@@ -12,15 +12,15 @@ exports.mysql = {
   // 单数据库信息配置
   client: {
     // host
-    host: 'localhost',
+    host: '172.21.0.4',
     // 端口号
     port: '3306',
     // 用户名
-    user: 'root',
+    user: 'gm_root',
     // 密码
-    password: 'root',
+    password: 'ifgame_gm',
     // 数据库名
-    database: 'gm',
+    database: 'db_gm',
   },
   // 是否加载到 app 上，默认开启
   app: true,
