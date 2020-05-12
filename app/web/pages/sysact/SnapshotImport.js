c9ecb9cabefe71e1d784ed6f91dc4a4e010cf0b0
elimsc <zhjiang.rtc@gmail.com>
2020-05-12 12:47:37 +0800

    角色快照导入--修复

--- app/web/pages/sysact/SnapshotImport.js
+++ app/web/pages/sysact/SnapshotImport.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Form, Input, Button, Card, message, Modal } from 'antd';
 
-import { gmIns, snapshotImport } from '../../service/sysact';
+import { snapshotImport } from '../../service/sysact';
 import { connect } from 'dva';
 
 @connect(({ global }) => ({ global }))
@@ -17,35 +17,23 @@ class SnapshotImport extends React.Component {
   handleSubmit = e => {
     e.preventDefault();
 
-    const { part_id, srvList } = this.props.global;
-
-    const cur_srv = srvList.filter(v => v.part_id === part_id)[0];
-    let part_name = '无';
-    if (cur_srv) {
-      part_name = cur_srv.part_name;
-    }
-
-    if (part_id === -1) {
-      message.error('请先选择要导入到的内网区服');
-      return;
-    }
-
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
         Modal.confirm({
           title: '确认操作',
-          content: `确认导入到: ${part_name}?`,
+          content: `确认执行该操作？`,
           onOk: () => {
             const from_part_ids = this.lines2Array(values['from_part_ids']);
             const from_guids = this.lines2Array(values['from_guids']);
             const to_guids = this.lines2Array(values['to_guids']);
             const keys = this.lines2Array(values['keys']);
+            const to_part_id = values['to_part_id'].trim();
             if (!(from_part_ids.length === from_guids.length
               && from_guids.length == to_guids.length && to_guids.length == keys.length)) {
               message.error('输入框中值的数目必须相同');
               return;
             }
-            snapshotImport({ from_guids, from_part_ids, to_guids, keys, to_part_id: part_id }).then(data => {
+            snapshotImport({ from_guids, from_part_ids, to_guids, keys, to_part_id }).then(data => {
               if (data.code === 0) {
                 message.success('操作成功');
                 this.props.form.resetFields();
@@ -88,8 +76,8 @@ class SnapshotImport extends React.Component {
     }
 
     return (
-      <Card title={`当前选中区服: ${part_name}`}>
-        {/* <Card> */}
+      // <Card title={`当前选中区服: ${part_name}`}>
+      <Card>
         <Form {...formItemLayout} onSubmit={this.handleSubmit} style={{ marginTop: 40 }}>
           <Form.Item label="外网角色所在区号">
             {getFieldDecorator('from_part_ids', {
@@ -117,6 +105,13 @@ class SnapshotImport extends React.Component {
               rules: [{ required: true, message: '不能为空' }]
             })(
               <Input.TextArea rows={5} placeholder="每行一个值" />
+            )}
+          </Form.Item>
+          <Form.Item label="内网要导入的机器">
+            {getFieldDecorator('to_part_id', {
+              rules: [{ required: true, message: '不能为空' }]
+            })(
+              <Input />
             )}
           </Form.Item>
           <Form.Item {...tailFormItemLayout}>
