ee11e7889f5293e12d6827cd3f5f4b5598b673bc
elimsc <zhjiang.rtc@gmail.com>
2021-03-30 18:52:20 +0800

    update

--- app/web/pages/player/components/gmact/Reissue.js
+++ app/web/pages/player/components/gmact/Reissue.js
@@ -79,7 +79,7 @@ class Reissue extends React.PureComponent {
       19: '648每日',
       20: '12元每日',
       21: '128每日',
-      22: '3240元',
+      22: '3240元每日',
     };
 
     const formItemLayout = {
