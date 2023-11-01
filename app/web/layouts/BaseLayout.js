089f0c7605219f582044ad5926248791eca65d7d
elimsc <zhaojiang.rtc@gmail.com>
2023-11-01 09:09:24 +0800

    feat: change ip port

--- app/web/layouts/BaseLayout.js
+++ app/web/layouts/BaseLayout.js
@@ -444,7 +444,7 @@ class BaseLayout extends React.Component {
                   placeholder="选择环境"
                 >
                   <Option value="">空</Option>
-                  <Option value="http://192.168.6.6:20143/">外网正式环境</Option>
+                  <Option value="http://172.21.0.3:20143/">外网正式环境</Option>
                   <Option value="http://192.168.1.205:20843/">
                     本地测试环境
                   </Option>
